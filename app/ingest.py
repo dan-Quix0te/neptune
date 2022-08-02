8582f439ae89680cc420af0ba96af1f0cf823333
dan-QuixOte <danquixote@protonmail.com>
2022-08-02 14:07:29 -0400

    ingest returns all file attributes

--- app/ingest.py
+++ app/ingest.py
@@ -6,10 +6,8 @@ import ffmpeg
 def ingestMedia(in_filename):
     probe = ffmpeg.probe(in_filename)
     video_stream = next((stream for stream in probe['streams'] if stream['codec_type'] == 'video'), None)
-    width = int(video_stream['width'])
-    height = int(video_stream['height'])
-    return width, height
+    return video_stream
 
 if __name__ == "__main__":
-    ingest('media/test.mp4')
+    ingestMedia('media/test.mp4')
 
