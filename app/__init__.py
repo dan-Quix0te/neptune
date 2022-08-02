04aacfb7049660aac7a9d82c30a7aa344b1bc7b0
dan-QuixOte <danquixote@protonmail.com>
2022-08-02 16:09:56 -0400

    logger

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 
 from flask import Flask
+from logging.handlers import RotatingFileHandler
+import os
 
 # local imports
 from config import app_config
@@ -14,4 +16,22 @@ def create_app(config_name):
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    # inject version var into all pages for footer
+    @app.context_processor
+    def injectVersion():
+        return dict(version=__version__)
+
+    if not app.debug:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/neptune.log', maxBytes=10240,
+                                           backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s %(name)s: %(message)s'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Neptune startup')
+
     return app
+
